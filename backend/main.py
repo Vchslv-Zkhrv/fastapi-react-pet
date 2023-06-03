48bb8bf1c640c1df5809294c2bc677e8e30c0476
Vchslv-Zkhrv <=>
2023-06-03 21:57:24 +0300

    lead CURD

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,9 @@ async def create_user(
     if db_user.all():
         raise _fastapi.HTTPException(status_code=400, detail="Email already in use")
 
-    return await _services.create_user(user, db)
+    user = await _services.create_user(user, db)
+
+    return await _services.create_token(user)
 
 
 @app.post("/api/token")
@@ -52,3 +54,50 @@ async def get_user(
     user: _schemas.User = _fastapi.Depends(_services.get_current_user)
 ):
     return user
+
+
+@app.post("/api/leads", response_model=_schemas.Lead)
+async def create_lead(
+    lead: _schemas.LeadCreate,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    return await _services.create_lead(user=user, db=db, lead=lead)
+
+
+@app.get("/api/leads", response_model=list[_schemas.Lead])
+async def get_leads(
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    return await _services.get_leads(user=user, db=db)
+
+
+@app.get("/api/leads/{lead_id}", status_code=200)
+async def get_lead(
+    lead_id: int,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    return await _services.get_lead(lead_id=lead_id, user=user, db=db)
+
+
+@app.delete("/api/leads/{lead_id}", status_code=204)
+async def delete_lead(
+    lead_id: int,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    await _services.delete_lead(lead_id, user, db)
+    return {"message": "Succefully Deleted"}
+
+
+@app.put("/api/leads/{lead_id}", status_code=200)
+async def update_lead(
+    lead_id: int,
+    lead: _schemas.LeadCreate,
+    user: _schemas.User = _fastapi.Depends(_services.get_current_user),
+    db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    await _services.update_lead(lead_id, lead, user, db)
+    return {"message": "Succefully Updated"}
